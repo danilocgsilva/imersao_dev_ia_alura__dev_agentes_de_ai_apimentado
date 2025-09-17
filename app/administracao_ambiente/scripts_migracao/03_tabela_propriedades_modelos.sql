e7151d008a478fe7d60819a2373763ce521f936b
Danilo Silva <contact@danilocgsilva.me>
2025-09-17 18:27:44 -0300

    wip: Salvando os detalhes dos modelos

--- app/administracao_ambiente/scripts_migracao/03_tabela_propriedades_modelos.sql
+++ app/administracao_ambiente/scripts_migracao/03_tabela_propriedades_modelos.sql
@@ -4,5 +4,5 @@ CREATE TABLE modelos_meta_dados(
     tipo_valor VARCHAR(32) NOT NULL,
     valor TEXT NOT NULL,
     modelo_id INT NOT NULL,
-    FOREIGN KEY (modelo_id) REFERENCES modelo(id)
+    FOREIGN KEY (modelo_id) REFERENCES modelos(id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- app/suporte/Banco.py
+++ app/suporte/Banco.py
@@ -7,6 +7,7 @@ import base64
 class Banco:
     def __init__(self):
         self._nome_banco = None
+        self._ultimo_id_inserido_banco = None
     
     @property
     def nome_banco(self):
@@ -15,6 +16,10 @@ class Banco:
     @nome_banco.setter
     def nome_banco(self, nome_banco: str):
         self._nome_banco = nome_banco
+        
+    @property
+    def ultimo_id_inserido(self):
+        return self._ultimo_id_inserido_banco
         
     def executar_sql(self, sql_script: str, scaping_replacements: tuple = ()):
         try:
@@ -28,6 +33,7 @@ class Banco:
                 mycursor.execute(f"USE {self.nome_banco}")
             mycursor.execute(sql_script, scaping_replacements)
             results = mycursor.fetchall()
+            self._ultimo_id_inserido_banco = mycursor.lastrowid
             return results
         except mysql.connector.Error as err:
             print(f"Error connecting to database or executing query: {err}")
@@ -40,33 +46,49 @@ class Banco:
     def registrar_modelos_disponiveis(self, modelos: list):
         
         self.registrar_request_de_busca_modelos_disponiveis(modelos)
+        id_registro_request = self.ultimo_id_inserido
         
         for modelo in modelos:
-            print("-----")
+            
+            self.salvar_modelo(modelo, id_registro_request)
+            id_modelo_iteracao = self.ultimo_id_inserido
+            
             for property in dir(modelo):
                 value = getattr(modelo, property)
                 value_type = type(value).__name__
                 
-                is_method = value_type == "method"
-                is_none_type = value_type == "NoneType"
-                is_builtin_function_or_method = value_type == "builtin_function_or_method"
-                is_method_wrapper = value_type == "method-wrapper"
-                is_tuple = value_type == "tuple"
-                is_dict = value_type == "dict"
-                is_data_class_params = value_type == "_DataclassParams"
-                is_type = value_type == "type"
+                # is_method = value_type == "method"
+                # is_none_type = value_type == "NoneType"
+                # is_builtin_function_or_method = value_type == "builtin_function_or_method"
+                # is_method_wrapper = value_type == "method-wrapper"
+                # is_tuple = value_type == "tuple"
+                # is_dict = value_type == "dict"
+                # is_data_class_params = value_type == "_DataclassParams"
+                # is_type = value_type == "type"
+                # is_list = value_type == "list"
+                
+                is_str = value_type == "str"
+                is_int = value_type == "int"
+                is_float = value_type == "float"
                 is_list = value_type == "list"
                 
-                if not is_none_type \
-                    and not is_builtin_function_or_method \
-                    and not is_method \
-                    and not is_method_wrapper \
-                    and not is_tuple \
-                    and not is_dict \
-                    and not is_data_class_params \
-                    and not is_type \
-                    and not is_list:
-                    print(f"   - {property}, {value_type} ,{value}")
+                # if not is_none_type \
+                #     and not is_builtin_function_or_method \
+                #     and not is_method \
+                #     and not is_method_wrapper \
+                #     and not is_tuple \
+                #     and not is_dict \
+                #     and not is_data_class_params \
+                #     and not is_type \
+                #     and not is_list:
+                #     print(f"   - {property}, {value_type} ,{value}")
+                
+                if is_str or is_int or is_float:
+                    # print(f"   - {property}, {value_type}, {value}")
+                    self.executar_sql("INSERT INTO modelos_meta_dados (campo, tipo_valor, valor, modelo_id) VALUES (%s, %s, %s, %s);", ())
+                if is_list:
+                    for entry in value:
+                        print(f"   - {property}, {value_type}, {entry}")
                     
     def registrar_request_de_busca_modelos_disponiveis(self, modelos):
         modelos_serializados = pickle.dumps(modelos)
@@ -74,3 +96,5 @@ class Banco:
         self.nome_banco = os.environ.get("NOME_BANCO")
         self.executar_sql(f"INSERT INTO busca_api (comando, retorno_serializado) VALUES (%s, %s);", ('GoogleApiWrapper().getModels()', modelos_serializados_base64,))
         
+    def salvar_modelo(self, modelo, id_registro_request: int):
+        self.executar_sql("INSERT INTO modelos (name, busca_id) VALUES (%s, %s)", (modelo.name, id_registro_request))
